991d3a01e577efcd13c0a30473e0170ab5c67290
Vicente Sanchez <Vikom.sanchez@gmail.com>
2024-01-09 17:34:45 -0300

    feat: get question excerpt

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs'
 import { Entity } from '../../core/entities/entity'
 import { UniqueEntityId } from '../../core/entities/unique-entity-id'
 import { Optional } from '../../core/types/optional'
@@ -14,13 +15,46 @@ interface QuestionProps {
 }
 
 export class Question extends Entity<QuestionProps> {
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get title() {
+    return this.props.title
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  get bestAnswerId() {
+    return this.props.bestAnswerId
+  }
+
+  get slug() {
+    return this.props.slug
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat('...')
+  }
+
   static create(
-    props: Optional<QuestionProps, 'createdAt'>,
+    props: Optional<QuestionProps, 'createdAt' | 'slug'>,
     id?: UniqueEntityId,
   ) {
     const question = new Question(
       {
         ...props,
+        slug: props.slug ?? Slug.createFromText(props.title),
         createdAt: new Date(),
       },
       id,
