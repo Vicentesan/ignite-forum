3f906d4b3ef36581c971ad52762063e11eccc4b7
Vicente Sanchez <Vikom.sanchez@gmail.com>
2024-01-10 18:46:52 -0300

    test: pagination and test refactor

--- src/domain/forum/application/use-cases/fetch-recent-questions.spec.ts
+++ src/domain/forum/application/use-cases/fetch-recent-questions.spec.ts
@@ -22,18 +22,26 @@ describe('Fetch Recent Questions', () => {
       MakeQuestion({ createdAt: new Date(2022, 0, 23) }),
     )
 
-    const questions = await sut.execute({
+    const { questions } = await sut.execute({
       page: 1,
     })
 
-    expect(questions).toEqual(
-      expect.objectContaining({
-        questions: expect.arrayContaining([
-          expect.objectContaining({ createdAt: new Date(2022, 0, 23) }),
-          expect.objectContaining({ createdAt: new Date(2022, 0, 20) }),
-          expect.objectContaining({ createdAt: new Date(2022, 0, 18) }),
-        ]),
-      }),
-    )
+    expect(questions).toEqual([
+      expect.objectContaining({ createdAt: new Date(2022, 0, 23) }),
+      expect.objectContaining({ createdAt: new Date(2022, 0, 20) }),
+      expect.objectContaining({ createdAt: new Date(2022, 0, 18) }),
+    ])
+  })
+
+  it('should be able to fetch paginated recent questions', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await inMemoryQuestionsRepository.create(MakeQuestion())
+    }
+
+    const { questions } = await sut.execute({
+      page: 2,
+    })
+
+    expect(questions).toHaveLength(2)
   })
 })
